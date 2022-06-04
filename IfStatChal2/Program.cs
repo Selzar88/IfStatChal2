527d8cf67235eaf51b4317cfbccc45bcb2301f66
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:32:52 +0100

    Add project files.

---
+++ IfStatChal2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IfStatChal2
+{
+    internal class Program
+    {
+
+        static int highscore = 300;
+        static string highscorePlayer = "Denis";
+
+        static void Main(string[] args)
+        {
+            CheckHighscore(250, "Maria");
+            CheckHighscore(315, "Michael");
+            CheckHighscore(360, "Denis");
+
+            Console.Read();
+        }
+
+        public static void CheckHighscore(int score , string playerName)
+        {
+            if(score > highscore)
+            {
+                highscore = score;
+                highscorePlayer = playerName;
+
+                Console.WriteLine("New highscore is " + score);
+                Console.WriteLine("it is now held by " + playerName);
+            }
+            else
+            {
+                Console.WriteLine("The old highscore could not be broken. It is still "
+                    + highscore + " and held by " + highscorePlayer);
+            }
+        }
+    }
+}
